ef2b0b02cce0229f8f3b8acc43e095d176f4713b
Lars Vilhuber <lars.vilhuber@cornell.edu>
2016-04-30 09:27:16 -0400

    Updating so all years/quarters are in the extract.

--- download_qwi.R
+++ download_qwi.R
@@ -1,12 +1,14 @@
 #' Define function to download, then subset the QWI data
 #'
 #' @param state upper or lower case string with two-letter (FIPS) state abbreviation
-#' @return a dataframe with the selected state's data
+#' @param qyear an integer or vector of years to choose
+#' @param qquarter an intenger or vector of quarters to choose
+#' @return a dataframe with the selected state's data, as subset
 #' @note You need to hard-code the selection criteria at this time
 #' @references
 #' \url{http://lehd.ces.census.gov/data/schema/V4.0.4/}
 #' @author Lars Vilhuber \email{lars.vilhuber@@cornell.edu}
-download_qwi <- function (state) {
+download_qwi <- function (state,qyear=NA,qquarter=NA) {
   
   qwifile <- paste("qwi",tolower(state),"sa_f_gs_ns_oslp_u",sep = "_")
   con <- gzcon(url(paste(urlbase,"/",tolower(state),"/",qwivintage,"/DVD-sa_f/",qwifile,".csv.gz", sep="")))
@@ -21,9 +23,17 @@ download_qwi <- function (state) {
                   ethnicity =="A0" & 
                   education == "E0" & 
                   firmage=="0" & 
-                  firmsize=="0" & 
-                  year == qwiyear & 
-                  quarter == qwiquarter )
+                  firmsize=="0" #&
+                  #year == qyear & 
+                  #quarter == qquarter 
+                )
+  if ( ! is.na(qyear) ) {
+    dat <- subset(dat,year %in% qyear )
+  }
+  if ( ! is.na(qquarter) ) {
+    dat <- subset(dat,quarter %in% qquarter )
+  }
+  
   # we ignore the flags for now
   dat <- dat[,names(dat)[-grep("^s[A-Z]",names(dat))]]
   # Add state name
